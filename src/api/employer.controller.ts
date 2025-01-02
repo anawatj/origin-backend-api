74cd885c21d58905b9fe9fd7346400c1d1aabc90
anawat1 <anawat@soilfish.co>
2025-01-02 15:14:41 +0700

    guard

--- src/api/employer.controller.ts
+++ src/api/employer.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, Query, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, Query, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor, NoFilesInterceptor } from "@nestjs/platform-express";
 import { EmployorUploadRequest } from "src/domain/model/employors/request/employer.upload.request";
 import { RetreveEmployerResponse } from "src/domain/model/employors/response/employer.retreve.response";
 import { EmployerUploadResponse } from "src/domain/model/employors/response/employer.upload.response";
 import { ResponseSuccess } from "src/domain/model/response/response.success";
+import { AuthGuard } from "src/infrastructure/common/auth.common";
 import { RetreveAllEmployerUsecase } from "src/usecases/employers/retreve.all.employers.usecases";
 import { UploadEmployerUsecase } from "src/usecases/employers/upload.employers.usecases";
 
@@ -16,12 +17,14 @@ export class EmployerController {
     ){}
 
     @Get()
+    @UseGuards(AuthGuard)
     async retreveAll(@Query("name") name:string|undefined):Promise<ResponseSuccess<RetreveEmployerResponse>>{
       const response =await  this.employerRetreveAllUsecase.execute(name);
       return response;
     }
 
     @Post('upload')
+    @UseGuards(AuthGuard)
     @UseInterceptors(FileInterceptor("file"))
     async uploadFile(@UploadedFile(
         new ParseFilePipeBuilder()
