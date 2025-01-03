61e0961d782c1b1c3bac07a1d758f1e5b3c503f0
anawat1 <anawat@soilfish.co>
2025-01-03 11:12:12 +0700

    reformat code

--- src/usecases/employers/upload.employers.usecases.ts
+++ src/usecases/employers/upload.employers.usecases.ts
@@ -11,50 +11,50 @@ import { User } from "src/infrastructure/entities/user";
 import { In, Not, Repository } from "typeorm";
 
 @Injectable()
-export class UploadEmployerUsecase{
+export class UploadEmployerUsecase {
     constructor(
         @InjectRepository(Employer)
-        private employerRepository:Repository<Employer>,
+        private employerRepository: Repository<Employer>,
         @InjectRepository(User)
-        private userRepository:Repository<User>,
-        private responseUtil:ResponseUtil<EmployerUploadResponse>
-    ){}
-    async execute(request:EmployorUploadRequest):Promise<ResponseSuccess<EmployerUploadResponse>>{
+        private userRepository: Repository<User>,
+        private responseUtil: ResponseUtil<EmployerUploadResponse>
+    ) { }
+    async execute(request: EmployorUploadRequest): Promise<ResponseSuccess<EmployerUploadResponse>> {
         const employee = new Employer();
-        employee.employerName=request.employerName;
-        employee.fileUrl="./files/"+request.file.filename;
-        const result = await this.employerRepository.save(employee);
-        const csvFile =readFileSync("./files/"+request.file.filename);
+        employee.employerName = request.employerName;
+        employee.fileUrl = "./files/" + request.file.filename;
+        await this.employerRepository.save(employee);
+        const csvFile = readFileSync("./files/" + request.file.filename);
         const csvData = csvFile.toString();
         const items = csvData.split("\\");
-        items.splice(0,1);
-        const users = items.map((item,index)=>{
+        items.splice(0, 1);
+        const users = items.map((item, index) => {
             const user = new User()
-            user.email=item[0];
-            user.fullName=item[1];
-            user.country=item[2];
-            user.birthDate=new Date(item[3]);
-            user.salary=parseFloat(item[4]);
+            user.email = item[0];
+            user.fullName = item[1];
+            user.country = item[2];
+            user.birthDate = new Date(item[3]);
+            user.salary = parseFloat(item[4]);
             return user;
         });
-        const userDbs = await this.userRepository.find({where:{email:In(users.map(user=>user.email))}});
-        const userUpdates = userDbs.map(user=>{
-            const u = users.find(u=>u.email==user.email);
-            user.country=u.country;
-            user.salary=u.salary;
-            user.accessType=AccessType.employer
+        const userDbs = await this.userRepository.find({ where: { email: In(users.map(user => user.email)) } });
+        const userUpdates = userDbs.map(user => {
+            const u = users.find(u => u.email == user.email);
+            user.country = u.country;
+            user.salary = u.salary;
+            user.accessType = AccessType.employer
             return user;
         });
 
-        await this.userRepository.save(userUpdates).catch((ex)=>new InternalServerErrorException(ex.message));
-        const dtcUsers = await this.userRepository.find({where:{email:Not(In(users.map(user=>user.email)))}});
-        const dtcUserUpdates = dtcUsers.map(user=>{
-            user.accessType=AccessType.dtc
+        await this.userRepository.save(userUpdates).catch((ex) => new InternalServerErrorException(ex.message));
+        const dtcUsers = await this.userRepository.find({ where: { email: Not(In(users.map(user => user.email))) } });
+        const dtcUserUpdates = dtcUsers.map(user => {
+            user.accessType = AccessType.dtc
             return user;
         });
-        await this.userRepository.save(dtcUserUpdates).catch((ex)=>new InternalServerErrorException(ex.message));
+        await this.userRepository.save(dtcUserUpdates).catch((ex) => new InternalServerErrorException(ex.message));
         const responseData = new EmployerUploadResponse();
-        responseData.message="Success";
-        return this.responseUtil.toResponse(responseData,HttpStatus.OK);
+        responseData.message = "Success";
+        return this.responseUtil.toResponse(responseData, HttpStatus.OK);
     }
 }
